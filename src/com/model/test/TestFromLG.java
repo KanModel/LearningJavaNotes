4d92820460521e0ea740220eb3dad72c9e6adaa7
Kan_Model <kgdwhsk@gmail.com>
2016-09-29 21:46:30 +0800

    测试各种细节问题

--- src/com/model/test/TestFromLG.java
+++ src/com/model/test/TestFromLG.java
@@ -1,11 +1,19 @@
 package com.model.test;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  * Created by kgdwhsk on 2016/9/29.
  */
 public class TestFromLG {
     public static void main(String args[]){
-        System.out.println(new TestFromLG().fun());
+//        System.out.println(new TestFromLG().fun());
+//        new TestFromLG().ArraysCompare();
+//        new TestFromLG().testString();
+        String s = null;
+        s = new TestFromLG().changeValueS(s);
+        System.out.println(s);
     }
 
     /*
@@ -13,5 +21,39 @@ public class TestFromLG {
     * */
     int fun(){
         return 20;
+    }
+
+    /*
+    * ==比较地址
+    * */
+    void ArraysCompare(){
+        int a1[] = {1,2,3};
+        int a2[] = {1,2,3};
+        if (a1 == a2){
+            System.out.println("same");
+        }else {
+            System.out.println("No");
+        }
+        if (Arrays.equals(a1,a2)){
+            System.out.println("same");
+        }else {
+            System.out.println("No");
+        }
+    }
+
+    /*
+    * 参数仅仅传递值
+    * */
+    void testString(){
+        String s = null;
+        changeValue(s);
+        System.out.println(s);
+    }
+    void changeValue(String a){
+        a = "GG";
+    }
+    String changeValueS(String a){
+        a = "GG";
+        return a;
     }
 }
