86bf57369caa3ea2d6a1b933663a79b58509e4fa
Kan_Model <kgdwhsk@gmail.com>
2016-10-02 09:00:39 +0800

    rtti

--- src/com/model/Main/MyRTTI.java
+++ src/com/model/Main/MyRTTI.java
@@ -1,8 +1,9 @@
 package com.model.Main;
 
 
-import com.model.TIJ4.typeinfos.toys.ToyTest;
+import com.model.TIJ4.typeinfos.toys.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
@@ -21,7 +22,9 @@ public class MyRTTI {
         * */
 //        new ToyTest().main(args);
 //        类字面常量();
-        泛化Class引用();
+//        泛化Class引用();
+//        new FilledList<CountedInteger>(CountedInteger.class).main(args);
+//        b567();
     }
 
     public static void myR(){
@@ -70,6 +73,26 @@ public class MyRTTI {
         genericI = Integer.class;
         Class<?> ic = int.class;
         ic = double.class;
+        Class<? extends Number> nu = int.class;
+        nu = double.class;
+        nu = Number.class;
+    }
+
+    public static void b567(){
+        Class<subC> ft = subC.class;
+        try {
+            subC c = ft.newInstance();
+            Class<? super subC> up = ft.getSuperclass();
+            /*
+            * 父类不能是抽象类？
+            *
+            * */
+            Object object = up.newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
 
@@ -128,6 +151,14 @@ class Cookie{
     }
 }
 
+class SuperC{
+
+}
+
+class subC extends SuperC{
+
+}
+
 class Initable{
     static final int sf = 47;
     static final int sf2 = MyRTTI.rand.nextInt(1000);
@@ -146,5 +177,39 @@ class Initable3{
     static int snf =74;
     static {
         System.out.println("三");
+    }
+}
+
+class CountedInteger{
+    private static long counter;
+    private final long id = counter++;
+
+    @Override
+    public String toString() {
+        return Long.toString(id);
+    }
+}
+
+class FilledList<T>{
+    private Class<T> type;
+    public FilledList(Class<T> type){
+        this.type = type;
+    }
+    public List<T> create(int nElements){
+        List<T> result = new ArrayList<T>();
+        try {
+            for (int i = 0; i <nElements; i++){
+                result.add(type.newInstance());
+            }
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+    public static void main(String[] args){
+        FilledList<CountedInteger> fl = new FilledList<CountedInteger>(CountedInteger.class);
+        List<CountedInteger> countedIntegers = fl.create(15);
+        System.out.println(countedIntegers);
+        System.out.println(countedIntegers.get(0).getClass().getName());
     }
 }
